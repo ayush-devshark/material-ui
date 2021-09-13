3a9eb3e610172d5bdaa23f13b3693231d6ca697a
ayush-devshark <ayushagarwal.dev@gmail.com>
2021-09-13 23:10:12 +0530

    part-2

--- src/App.js
+++ src/App.js
@@ -10,21 +10,25 @@ import {
     Grid,
     Toolbar,
     Container,
+    Button,
 } from '@material-ui/core';
 import { PhotoCamera } from '@material-ui/icons';
+import useStyles from './styles';
 
+const array = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 function App() {
+    const classes = useStyles();
     return (
         <>
             <CssBaseline />
             <AppBar position='relative'>
                 <Toolbar>
-                    <PhotoCamera />
+                    <PhotoCamera className={classes.icon} />
                     <Typography variant='h6'>Photo Album</Typography>
                 </Toolbar>
             </AppBar>
             <main>
-                <div>
+                <div className={classes.container}>
                     <Container maxWidth='sm'>
                         <Typography
                             variant='h2'
@@ -46,8 +50,57 @@ function App() {
                             error consequuntur, atque quas veniam? Quod,
                             laboriosam nesciunt? Consequatur, alias incidunt.
                         </Typography>
+                        <div className={classes.buttons}>
+                            <Grid container spacing={2} justify='center'>
+                                <Grid item>
+                                    <Button variant='contained' color='primary'>
+                                        See My Photos
+                                    </Button>
+                                </Grid>
+                                <Grid item>
+                                    <Button variant='outlined' color='primary'>
+                                        Secondary Action
+                                    </Button>
+                                </Grid>
+                            </Grid>
+                        </div>
                     </Container>
                 </div>
+                <Container className={classes.cardGrid} maxWidth='md'>
+                    <Grid container spacing={4}>
+                        {array.map(cardNum => (
+                            <Grid item key={cardNum} xs={12} sm={6} md={4}>
+                                <Card className={classes.card}>
+                                    <CardMedia
+                                        className={classes.cardMedia}
+                                        image='https://source.unsplash.com/random'
+                                        title='Image title'
+                                    />
+                                    <CardContent
+                                        className={classes.cardContent}
+                                    >
+                                        <Typography gutterBottom variant='h5'>
+                                            Heading
+                                        </Typography>
+                                        <Typography>
+                                            This is a media card, you can use
+                                            this section to describe the
+                                            content.
+                                        </Typography>
+                                    </CardContent>
+                                    <CardActions>
+                                        <Button size='small' color='primary'>
+                                            View
+                                        </Button>
+                                        <Button size='small' color='primary'>
+                                            Edit
+                                        </Button>
+                                    </CardActions>
+                                </Card>
+                            </Grid>
+                        ))}
+                    </Grid>
+                </Container>
             </main>
         </>
     );
